912430b1557bbd69bcec98ef52435849c3c527b1
Andreas Dyballa <adyballa@gmx.de>
2016-08-09 03:00:20 +0200

    add decorator-eq transpilation

---
+++ dist/node_modules/decorator-eq/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./eq.config"));
+__export(require("./eq.interface"));
+__export(require("./eq.or.typeclass"));
+__export(require("./eq.typeclass"));
+//# sourceMappingURL=index.js.map
