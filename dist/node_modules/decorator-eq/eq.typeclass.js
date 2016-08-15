4810df45c79346e379fd335f7acfa73bc47dd878
Andreas Dyballa <adyballa@gmx.de>
2016-08-15 20:57:16 +0200

    revert: adding fields instead of creating them while implementing

--- dist/node_modules/decorator-eq/eq.typeclass.js
+++ dist/node_modules/decorator-eq/eq.typeclass.js
@@ -30,7 +30,7 @@ var Eq = (function () {
             }
         }
         if (config) {
-            config.fields = config.fields.concat(_f);
+            config.fields = _f;
         }
     };
     Eq.field = function (props) {
